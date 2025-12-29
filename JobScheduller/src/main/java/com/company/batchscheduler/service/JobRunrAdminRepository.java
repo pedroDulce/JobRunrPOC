625d435532e8bbb9b2c9d510aacad4ee2f56deb1
pedroDulce <pedro.dulce@gmail.com>
2025-12-29 20:56:00 +0100

    mejoras

--- JobScheduller/src/main/java/com/company/batchscheduler/service/JobRunrAdminRepository.java
+++ JobScheduller/src/main/java/com/company/batchscheduler/service/JobRunrAdminRepository.java
@@ -1,5 +1,6 @@
 package com.company.batchscheduler.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
@@ -7,6 +8,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class JobRunrAdminRepository {
 
     private static final List<String> STATUS_POSIBLES = List.of("ENQUEUED", "SCHEDULED","PROCESSING",
@@ -21,14 +23,23 @@ public class JobRunrAdminRepository {
     public int updateJobState(UUID jobId, String newState) {
         if (STATUS_POSIBLES.contains(newState)) {
             String sql = """
-                        UPDATE jobrunr_jobrunr_jobs
-                        SET
-                            state = ?,
-                            updated_at = now(),
-                            version = version + 1
-                        WHERE id = ?
-                    """;
-            return jdbcTemplate.update(sql, newState, jobId);
+                UPDATE jobrunr_jobrunr_jobs
+                SET state = ?,
+                    updatedat = now(),
+                    version = version + 1
+                WHERE id = ?
+            """;
+
+            int updated = jdbcTemplate.update(sql,
+                    newState,
+                    jobId.toString() // PASAR COMO STRING, no UUID
+            );
+
+            if (updated == 0) {
+                log.warn("No se encontró job con id {}", jobId);
+            } else {
+                log.info("Job {} actualizado a estado {}", jobId, newState);
+            }
         }
         throw new RuntimeException("Estado: " + newState + " no permitido. Solo estados: " + STATUS_POSIBLES.toArray());
     }
