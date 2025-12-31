88deabbbb41510393ae2b443cd1b0a0a205b5272
pedroDulce <pedro.dulce@gmail.com>
2025-12-31 15:29:55 +0100

    mejoras

---
+++ JobScheduller/src/main/java/com/company/batchscheduler/exception/RemoteJobException.java
@@ -0,0 +1,9 @@
+package com.company.batchscheduler.exception;
+
+public class RemoteJobException extends Exception {
+
+    public RemoteJobException(String error) {
+        super(error);
+    }
+
+}
