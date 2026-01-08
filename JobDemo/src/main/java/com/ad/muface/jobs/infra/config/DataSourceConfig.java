8e2f936d332d6450bbad49a57b7f9c1127dc38d2
pedroDulce <pedro.dulce@gmail.com>
2026-01-08 09:59:49 +0100

    mejoras

--- JobDemo/src/main/java/com/ad/muface/jobs/infra/config/DataSourceConfig.java
+++ JobDemo/src/main/java/com/ad/muface/jobs/infra/config/DataSourceConfig.java
@@ -1,57 +1,69 @@
 package com.ad.muface.jobs.infra.config;
 
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfig {
 
-    // ==================== Datasource Batch (Spring Batch Metadata) ====================
-    @Bean
     @Primary
-    @ConfigurationProperties("spring.datasource.batch")
-    public DataSourceProperties batchDataSourceProperties() {
-        return new DataSourceProperties();
-    }
-
-    @Bean(name = "dataSource")
-    @Primary
-    @ConfigurationProperties("spring.datasource.batch.hikari")
-    public DataSource batchDataSource(@Qualifier("batchDataSourceProperties") DataSourceProperties props) {
-        return props.initializeDataSourceBuilder().build();
-    }
-
-    @Bean(name = "transactionManager")
-    @Primary
-    public DataSourceTransactionManager batchTransactionManager(@Qualifier("dataSource") DataSource dataSource) {
-        return new DataSourceTransactionManager(dataSource);
-    }
-
-    // Si necesitas uno para JDBC puro sobre la base de negocio
-    @Bean(name = "businessJdbcTransactionManager")
-    public DataSourceTransactionManager businessJdbcTransactionManager(@Qualifier("businessDataSource") DataSource dataSource) {
-        return new DataSourceTransactionManager(dataSource);
-    }
-
-    // ==================== Datasource de negocio ====================
-    @Bean
-    @ConfigurationProperties("spring.datasource.business")
-    public DataSourceProperties businessDataSourceProperties() {
-        return new DataSourceProperties();
-    }
-
     @Bean(name = "businessDataSource")
-    @ConfigurationProperties("spring.datasource.business.hikari")
-    public DataSource businessDataSource(@Qualifier("businessDataSourceProperties") DataSourceProperties props) {
-        return props.initializeDataSourceBuilder().build();
+    @ConfigurationProperties(prefix = "spring.datasource.business")
+    public DataSource businessDataSource() {
+        return DataSourceBuilder.create().build();
     }
 
-}
+    // DataSource para Spring Batch
+    @Bean(name = "batchDataSource")
+    @ConfigurationProperties(prefix = "spring.batch.datasource")
+    public DataSource batchDataSource() {
+        return DataSourceBuilder.create().build();
+    }
 
+    // Inicializador del DataSource de batch: crea las tablas
+    @Bean
+    public DataSourceInitializer batchDataSourceInitializer(@Qualifier("batchDataSource") DataSource batchDataSource,
+                                                            ResourceLoader resourceLoader) {
+        DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(batchDataSource);
+
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        // Cargar el script de creación de tablas para PostgreSQL desde el classpath
+        populator.addScript(resourceLoader.getResource("classpath:org/springframework/batch/core/schema-postgresql.sql"));
+        populator.setContinueOnError(true); // Continuar si las tablas ya existen
+        populator.setIgnoreFailedDrops(true);
+
+        initializer.setDatabasePopulator(populator);
+        return initializer;
+    }
+
+    @Bean
+    public JobRepository jobRepository(@Qualifier("batchDataSource") DataSource batchDataSource) throws Exception {
+        JobRepositoryFactoryBean factory = new JobRepositoryFactoryBean();
+        factory.setDataSource(batchDataSource);
+        factory.setTransactionManager(batchTransactionManager(batchDataSource));
+        factory.setDatabaseType("POSTGRES");
+        factory.setTablePrefix("BATCH_");
+        factory.setMaxVarCharLength(1000);
+        factory.afterPropertiesSet();
+        return factory.getObject();
+    }
+
+    @Bean
+    public PlatformTransactionManager batchTransactionManager(@Qualifier("batchDataSource") DataSource batchDataSource) {
+        return new DataSourceTransactionManager(batchDataSource);
+    }
+}
